9d300d42154108a113f72937875749917aa11700
konh1122 <153785092+konh1122@users.noreply.github.com>
2025-08-18 01:32:26 +0700

    web_view_v2

--- server/server.js
+++ server/server.js
@@ -6,7 +6,8 @@ import videoRoutes from './routes/videos.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
